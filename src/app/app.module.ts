ce2d23ed11eb4b6095a1befdd35105d99eaba87b
João Paulo <joaowais@gmail.com>
2024-01-26 19:24:36 -0300

    adicionado card

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,9 +7,10 @@ import { BannerComponent } from './components/banner/banner.component';
 import { HeaderComponent } from './components/header/header.component';
 import { HomeComponent } from './pages/home/home.component';
 import { CardListComponent } from './components/card-list/card-list.component';
+import { CardComponent } from './components/card/card.component';
 
 @NgModule({
-  declarations: [AppComponent, HeaderComponent, HomeComponent, BannerComponent, CardListComponent],
+  declarations: [AppComponent, HeaderComponent, HomeComponent, BannerComponent, CardListComponent, CardComponent],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
